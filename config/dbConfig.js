7abe628ad0ec7faee2e70b771ef25bfe239f1be9
Tee <cickpoo0121@gmail.com>
2020-11-10 04:05:25 +0700

    testdeploy

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,14 @@
-let config = {
-    host    : 'localhost',
-    user    : 'root',
-    password: '',
+// let config = {
+//     host    : 'localhost',
+//     user    : 'root',
+//     password: '',
+//     database: 'will3'
+//   };
+// mysql://b9700c9627b485:100ee018@us-cdbr-east-02.cleardb.com/heroku_cbd38efa52b81e8?reconnect=true
+  let config = {
+    host    : 'us-cdbr-east-02.cleardb.com',
+    user    : 'b9700c9627b485',
+    password: '100ee018',
     database: 'will3'
   };
    
