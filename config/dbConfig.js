75c07ed412bcfebc83c2abe20d1b1d80449ebbfa
Tee <cickpoo0121@gmail.com>
2020-11-29 23:19:15 +0700

    deploy

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,16 +1,16 @@
-// let config = {
-//     host    : 'localhost',
-//     user    : 'root',
-//     password: '',
-//     database: 'will3'
-//   };
-// mysql://b9700c9627b485:100ee018@us-cdbr-east-02.cleardb.com/heroku_cbd38efa52b81e8?reconnect=true
 let config = {
-  host: 'us-cdbr-east-02.cleardb.com',
-  user: 'b9700c9627b485',
-  password: '100ee018',
-  database: 'heroku_cbd38efa52b81e8',
-  port: '3306'
-};
+    host    : 'localhost',
+    user    : 'root',
+    password: '',
+    database: 'will3'
+  };
+// mysql://b9700c9627b485:100ee018@us-cdbr-east-02.cleardb.com/heroku_cbd38efa52b81e8?reconnect=true
+// let config = {
+//   host: 'us-cdbr-east-02.cleardb.com',
+//   user: 'b9700c9627b485',
+//   password: '100ee018',
+//   database: 'heroku_cbd38efa52b81e8',
+//   port: '3306'
+// };
 
 module.exports = config;
